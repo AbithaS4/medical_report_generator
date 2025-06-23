48cf6713d3dff9b0af1a7ea8309e9d5d317d6ac3
Nivethitha237 <nivethithadhascm@email.com>
2025-06-23 20:59:48 +0530

    Commit Changes

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
+from fastapi.middleware.cors import CORSMiddleware
+
 from .patients import router as patient_router
 from .medical_rpt import router as report_router
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend address
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(patient_router, prefix="/patients")
 app.include_router(report_router, prefix="/reports")
